b463d4d5622d279dfcd80eb19e19d87e62703d8d
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2022-07-22 22:20:52 +0700

    fix: GKE module URL

--- gke.tf
+++ gke.tf
@@ -1,5 +1,5 @@
 module "gke" {
-  source = "git@github.com:iamdejan/terraform-k8s-module?ref=v0.1.2"
+  source = "git::https://github.com/iamdejan/terraform-k8s-module?ref=v0.1.2"
 
   region     = local.region
   project_id = local.project
