aeed7d6349f2a553e07c16412aa728e0f4d15753
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2022-07-22 22:24:25 +0700

    fix: remove 'outputs'

--- agones.tf
+++ agones.tf
@@ -4,8 +4,8 @@ module "agones" {
   agones_version         = ""
   values_file            = ""
   feature_gates          = ""
-  host                   = module.gke.outputs.kubernetes_cluster_host
-  token                  = module.gke.outputs.project_access_token
-  cluster_ca_certificate = module.gke.outputs.cluster_ca_certificate
+  host                   = module.gke.kubernetes_cluster_host
+  token                  = module.gke.project_access_token
+  cluster_ca_certificate = module.gke.cluster_ca_certificate
   log_level              = "info"
 }
--- gke.tf
+++ gke.tf
@@ -2,5 +2,5 @@ module "gke" {
   source = "git::https://github.com/iamdejan/terraform-k8s-module?ref=v0.1.2"
 
   region     = local.region
-  project_id = local.project
+  project_id = local.project_id
 }
